c091844c7a38d757aa17fa2e990e1ce63dfd9c9a
TheCharismaticEnigma <avishkar2002@mail.com>
2023-08-07 21:11:20 +0530

    Enhance: Setup Root Route w/ React Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
 import './global.css'; // contains global styles for the application.
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import { chakraTheme } from './utils/chakraCustomTheme.ts';
+import { RouterProvider } from 'react-router-dom';
+import router from './router.tsx';
 
 const queryClient = new QueryClient();
 
@@ -15,7 +16,8 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <ChakraProvider theme={chakraTheme}>
       <ColorModeScript initialColorMode={chakraTheme.config.initialColorMode} />
       <QueryClientProvider client={queryClient}>
-        <App />
+        {/* <App /> */}
+        <RouterProvider router={router} />
         <ReactQueryDevtools />
       </QueryClientProvider>
     </ChakraProvider>
