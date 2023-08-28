725a659b4e68380461969eb4a67ff234314bc19a
TheCharismaticEnigma <avishkar2002@mail.com>
2023-08-28 20:36:10 +0530

    Add Related Artists Skeleton

--- src/components/Artists.tsx
+++ src/components/Artists.tsx
@@ -7,20 +7,27 @@ import getUniqueArtists from '../utils/getUniqueArtists';
 import ArtistListBar from './ArtistListBar';
 import Wrapper from './Wrapper';
 import { initialArtists } from '../initialData/initialArtists';
+import ArtistListBarSkeleton from '../skeletons/ArtistListBarSkeleton';
 
 const Artists = () => {
   const { artistId } = useSpotifyQueryStore((s) => s.spotifyQuery);
 
-  const { data: relatedArtists, error: relatedArtistsError } =
-    useRelatedArtists(artistId);
+  const {
+    data: relatedArtists,
+    error: relatedArtistsError,
+    isLoading: relatedArtistsAreLoading,
+  } = useRelatedArtists(artistId);
 
-  const { data: selectedArtist } = useArtist(artistId);
+  const { data: selectedArtist, isLoading: artistIsLoading } =
+    useArtist(artistId);
 
   let artists: Artist[] = [];
   if (selectedArtist && relatedArtists)
     artists = [...getUniqueArtists(selectedArtist, relatedArtists)];
 
   if (relatedArtistsError) artists = initialArtists;
+
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   return (
     <Wrapper>
@@ -33,6 +40,11 @@ const Artists = () => {
         height={'auto'}
       >
         <Flex as="ul" direction={'column'} fontSize={'1.8rem'} gap={'0.5rem'}>
+          {(relatedArtistsAreLoading || artistIsLoading) &&
+            skeletons.map((skeleton) => (
+              <ArtistListBarSkeleton key={skeleton} />
+            ))}
+
           {artists?.map((artist) => (
             <li key={artist.name}>
               <ArtistListBar artist={artist} />
---
+++ src/skeletons/ArtistListBarSkeleton.tsx
@@ -0,0 +1,19 @@
+import { SkeletonCircle, SkeletonText, Box } from '@chakra-ui/react';
+
+const ArtistListBarSkeleton = () => {
+  return (
+    <Box
+      mb={'1rem'}
+      borderRadius={'1rem'}
+      padding="6"
+      boxShadow="lg"
+      bg="gray.500"
+      gap={'1rem'}
+    >
+      <SkeletonCircle size={'10'} />
+      <SkeletonText mt={4} noOfLines={2} spacing="4" skeletonHeight="2" />
+    </Box>
+  );
+};
+
+export default ArtistListBarSkeleton;
